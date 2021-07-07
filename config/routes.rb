9082c5a0268f4c85fceb564f91a41d81abd9c661
isabellagh <isabellagh88@gmail.com>
2021-07-07 16:57:35 -0400

    resources for user, client, workout + namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+
+  namespace :api do
+    namespace v1 do
+      resources :users
+
+      resources :clients do
+        resources :workouts
+      end
+    end
+  end
+  
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
